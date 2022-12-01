b503bcbcccee7254dda43749d7e5975ef07d9c50
Cassandra <cassandrabunschoten@gmail.com>
2022-12-01 07:08:06 +0100

    set up project - add day 1

---
+++ src/main/scala/aoc1.scala
@@ -0,0 +1,16 @@
+package aoc2020 
+
+import scala.io.Source
+
+object AOC1 extends App:
+    val input = Source
+        .fromFile("src/main/resources/input_aoc1.txt").mkString
+        
+    val outputPart1 = input.split("\n\n").toList.map(_.split("\n").map(_.toInt).toList).filter(!_.isEmpty).map(_.sum)
+
+    val outputPart2 = outputPart1.sorted.takeRight(3).sum
+
+
+    println("Answer to part 1: " + outputPart1.max)
+
+    println("Answer to part 2: " + outputPart2)
