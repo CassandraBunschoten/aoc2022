fa5d154b2c189f3d0e24b681788a9964c2edec64
Cassandra <cassandrabunschoten@gmail.com>
2022-12-02 16:39:42 +0100

    tidy up day1

--- src/main/scala/aoc1.scala
+++ src/main/scala/aoc1.scala
@@ -3,10 +3,12 @@ package aoc2022
 import scala.io.Source
 
 object AOC1 extends App:
-    val input = Source
-        .fromFile("src/main/resources/input_aoc1.txt").mkString
-        
-    val outputPart1 = input.split("\n\n").toList.map(_.split("\n").map(_.toInt).toList).filter(!_.isEmpty).map(_.sum)
+    val outputPart1 = Source
+        .fromFile("src/main/resources/input_aoc1.txt")
+        .mkString
+        .split("\n\n")
+        .map(_.split("\n").map(_.toInt))
+        .map(_.sum)
 
     val outputPart2 = outputPart1.sorted.takeRight(3).sum
 
