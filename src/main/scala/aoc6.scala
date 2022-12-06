305f26c825aafa19bdce030962c93a2be4b7bf90
Cassandra <cassandrabunschoten@gmail.com>
2022-12-06 06:46:22 +0100

    day 6

---
+++ src/main/scala/aoc6.scala
@@ -0,0 +1,18 @@
+package aoc2022
+
+import scala.io.Source
+
+
+object AOC6 extends App:
+  val input = Source.fromFile("src/main/resources/input_aoc6.txt").getLines.toList
+
+  def findFirstDistinctGroup(inputList: List[String], window: Int) = 
+    inputList
+      .map(_.sliding(window)
+      .zipWithIndex.filter{case (letters, index) => (letters.distinct.size == window)}
+      .map{case (letters, index) => (index + window)}
+      .min)
+      .sum
+  
+  println("Answer to part 1: " + findFirstDistinctGroup(input, 4))
+  println("Answer to part 2: " + findFirstDistinctGroup(input, 14))
