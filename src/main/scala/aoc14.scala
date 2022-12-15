f181d91d83bef61a9512925582a4e1302d31a7d2
Cassandra <cassandrabunschoten@gmail.com>
2022-12-15 22:58:49 +0100

    too tired to care about how ugly my code is rn

---
+++ src/main/scala/aoc14.scala
@@ -0,0 +1,98 @@
+import scala.io.Source
+import scala.collection.mutable.ArrayBuffer
+import java.io._
+
+val pattern = "([0-9]+),([0-9]+)".r
+
+def drawMap(i: List[String], startCoord: List[Coord]): List[Coord] = {
+  i match {
+    case s"$x,$y" :: next => drawMap(next, drawTo(startCoord, Coord(x.toInt, y.toInt)))
+    case Nil => startCoord
+    case _ => sys.error("uhoh")
+  }
+}
+
+def drawTo(coord1: List[Coord], coord2: Coord): List[Coord] = {
+  val (difx, dify) = coord1.head.difference(coord2)
+  if (difx == dify) {
+    coord1
+  } else if (difx > 0) {
+    drawTo(coord1.head.copy(x = coord1.head.x - 1) :: coord1, coord2)
+  } else if (difx < 0) {
+    drawTo(coord1.head.copy(x = coord1.head.x + 1) :: coord1, coord2)
+  } else if (dify > 0) {
+    drawTo(coord1.head.copy(y = coord1.head.y - 1) :: coord1, coord2)
+  } else {
+    drawTo(coord1.head.copy(y = coord1.head.y + 1) :: coord1, coord2)
+  }
+}
+
+def checkMovement(p1: Boolean, cave: ArrayBuffer[ArrayBuffer[Char]], minX: Int, maxX: Int, maxY: Int, coord: Coord = Coord(500, 1), acc: Int = 0): Int = {
+  if (coord.x - minX + 1 > maxX) {
+    if (p1) {
+      acc
+    } else {
+      val newCave = cave.map(r => r :+ '.')
+      newCave(maxY)(coord.x - minX + 1) = '#'
+      checkMovement(p1, newCave, minX, maxX + 1, maxY, coord = coord.copy(x = coord.x, y = coord.y), acc)
+    }
+  } else if (coord.x - minX - 1 < 0) {
+    if (p1) {
+      acc
+    } else {
+      val newCave = cave.map(r => '.' +: r)
+      newCave(maxY)(0) = '#'
+      checkMovement(p1, newCave, minX - 1, maxX, maxY, coord = coord.copy(x = coord.x, y = coord.y), acc)
+    }
+  } else if (coord.y + 1 == maxY && p1) {
+    acc
+  } else if (cave(coord.y + 1)(coord.x - minX) == '.') {
+    checkMovement(p1, cave, minX, maxX, maxY, coord.copy(y = coord.y + 1), acc)
+  } else if (cave(coord.y + 1)(coord.x - minX - 1) == '.') {
+    checkMovement(p1, cave, minX, maxX, maxY, coord.copy(x = coord.x - 1, y = coord.y + 1), acc)
+  } else if (cave(coord.y + 1)(coord.x - minX + 1) == '.') {
+    checkMovement(p1, cave, minX, maxX, maxY, coord.copy(x = coord.x + 1, y = coord.y + 1), acc)
+  } else {
+    cave(coord.y)(coord.x - minX) = 'o';
+    if (!p1 && (cave(0)(500 - minX) == 'o')) {
+      acc
+    } else {
+      checkMovement(p1, cave, minX, maxX, maxY, acc = acc + 1)
+    }
+  }
+}
+
+def visualise(a: ArrayBuffer[ArrayBuffer[Char]]) =
+  println(a.map(_.mkString("")).mkString("\n"))
+
+case class Coord(x: Int, y: Int):
+  def difference(coord2: Coord): (Int, Int) =
+    (this.x - coord2.x, this.y - coord2.y)
+
+object Day14 extends App :
+  val input = Source.fromFile("src/main/resources/input_example.txt")
+    .getLines.map(x => x.split(" -> ").toList)
+    .flatMap(l => {
+      val pattern(x, y) = l.head
+      drawMap(l.tail, List(Coord(x.toInt, y.toInt)))
+    }).toList
+
+  val minX = input.map(_.x).min
+  val maxX = input.map(_.x).max - minX + 1
+  val maxY = input.map(_.y).max + 1
+
+  val cave = ArrayBuffer.fill(maxY, maxX)('.')
+  input.map(c => cave(c.y)(c.x - minX) = '#')
+
+  val newCave = ArrayBuffer.fill(maxY, maxX)('.')
+  input.map(c => newCave(c.y)(c.x - minX) = '#')
+  val emptyLayer = ArrayBuffer.fill(maxX)('.')
+  val rockLayer = ArrayBuffer.fill(maxX)('#')
+  newCave += emptyLayer += rockLayer
+
+  val answerP1 = checkMovement(p1 = true, cave, minX, maxX, maxY)
+  val answerP2 = checkMovement(p1 = false, newCave, minX, maxX, maxY + 1)
+
+  visualise(cave)
+
+  println(answerP1)
---
+++ src/main/scala/aoc15.scala
@@ -0,0 +1,61 @@
+package aoc2022_15
+
+import scala.io.Source
+
+def parse(l: String) =
+  l match {
+    case s"Sensor at x=$x1, y=$y1: closest beacon is at x=${x2}, y=${y2}" => SensorBeaconPair(sensor = Coord(x1.toInt, y1.toInt), beacon = Coord(x2.toInt, y2.toInt))
+    case _  => sys.error("uhoh")
+  }
+
+case class Interval(lower: Coord, upper: Coord)
+
+case class Diamond(lowerX: Int, upperX: Int, lowerY: Int, upperY: Int)
+
+case class Coord(x: Int, y: Int):
+  def manhattansDistance(coord2: Coord): Int =
+    math.abs(this.x - coord2.x) + math.abs(this.y - coord2.y)
+
+case class SensorBeaconPair(sensor: Coord, beacon: Coord):
+  val manDist = sensor.manhattansDistance(beacon)
+  def findIntervalForRow(row: Int): Option[Interval] = {
+    val colInt = Interval(Coord(sensor.x, sensor.y - manDist), Coord(sensor.x, sensor.y + manDist))
+
+    def intervalForManDistance: Interval = {
+      val dist = manDist - math.abs(row - this.sensor.y)
+      Interval(Coord(sensor.x - dist, row), Coord(sensor.x + dist, row))
+    }
+
+    if (row <= colInt.upper.y && row >= colInt.lower.y) {
+      Some(intervalForManDistance)
+    } else {
+      None
+    }
+  }
+
+var answer: (Int, Int) = (-1, -1)
+def findHoleInRow(li: List[SensorBeaconPair], row: Int, rowMax: Int): Unit =
+  val intervalsRow = li.foldLeft(List[Option[Interval]]())((acc, sbp) => sbp.findIntervalForRow(row) :: acc).filter(oi => oi.isDefined).map(_.get)
+
+  val sortedRanges = intervalsRow.sortBy(x => x.lower.x) //.map(x => x.lower.x to x.upper.x + 1)
+
+  sortedRanges.foldLeft(0)((acc, x) => if (x.lower.x > acc + 1) {answer = (acc + 1, row)) ; math.max(acc, x.upper.x)} else {math.max(acc, x.upper.x)})
+  if (row < rowMax) {
+    findHoleInRow(li, row + 1, rowMax)
+  } else println("bloop")
+
+object Day15 extends App:
+  val input = Source.fromFile("src/main/resources/input_aoc15.txt").getLines.map(parse).toList
+
+  val intervalsP1 = input.foldLeft(List[Option[Interval]]())((acc, sbp) => (sbp.findIntervalForRow(2000000) :: acc)).filter(_.isDefined).distinct
+
+  val beacons = input.filter(x => x.beacon.y == 2000000).map(b => b.beacon).distinct.size
+  val lowerX = intervalsP1.map(i => i.get.lower.x).min
+  val upperX = intervalsP1.map(i => i.get.upper.x).max
+
+  val answerP1 = if (lowerX < 0 ) {(upperX - lowerX) - beacons + 1} else {(upperX - lowerX) - beacons}
+
+  val answerP2 = (findHoleInRow(input, 0, 4000000))
+
+  println(answerP1)
+  println(answer)
